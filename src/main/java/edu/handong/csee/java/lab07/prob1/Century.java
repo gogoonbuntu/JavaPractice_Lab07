8385a5dde515f609776191510736e7eada00c852
GGBT <tmddud333@naver.com>
2018-04-02 22:27:22 +0900

    ok2

--- src/main/java/edu/handong/csee/java/lab07/prob1/Century.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/Century.java
@@ -2,16 +2,20 @@ package edu.handong.csee.java.lab07.prob1;
 
 import java.util.Scanner;
 
+/**
+ * @author SeungYoung Jung.
+ * I tried to use different method from doing Math, but using String.
+ */
 public class Century {
 	public static void main(String [] args) {
 		int whatyear;
 		System.out.println("Input year :");
-		Scanner kbd = new Scanner(System.in);
+		Scanner kbd = new Scanner(System.in); //Receive input.
 		whatyear = kbd.nextInt();
 		kbd.close();
-		CenturyMath ctm = new CenturyMath();
-		ctm.CentryRcv(whatyear);
-		ctm.CentryRes();
+		CenturyMath ctm = new CenturyMath(); //Initiating constructor.
+		ctm.CentryRcv(whatyear); //give the input to the constructor.
+		ctm.CentryRes(); //Prints out the result.
 	}
 	
 }
--- src/main/java/edu/handong/csee/java/lab07/prob1/CenturyMath.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/CenturyMath.java
@@ -2,26 +2,25 @@ package edu.handong.csee.java.lab07.prob1;
 
 public class CenturyMath {
 	int year;
-	public void CentryMath()
+	public void CentryMath() // initialize the "year"
 	{
 		year=0;
 	}
-	public void CentryRcv(int yeartime)
+	public void CentryRcv(int yeartime) //Rcv stands for Receive.
 	{
 		year = yeartime;
 	}
-	public void CentryRes()
+	public void CentryRes() //Res stands for Result.
 	{
 		int ctn = 0, ndn=0;
-		String i = String.valueOf(year);
-		String ct = i.substring(0, i.length()-2);
-		String nd = i.substring(2,4);
-		System.out.println(ct+"and"+nd);
-		ctn = Integer.parseInt(ct);
-		ndn = Integer.parseInt(nd);
-		if(ndn==0){}
-		else {ctn++;}
-		System.out.println(year+" is "+ctn+"th century.");
+		String i = String.valueOf(year); //Change the "year" into String.
+		String ct = i.substring(0, i.length()-2); //Read First two number from the string.
+		String nd = i.substring(2,4); //Read last two number from the string.
+		ctn = Integer.parseInt(ct); //then Integerize the substrings.
+		ndn = Integer.parseInt(nd); //the second one too.
+		if(ndn==0){} //if last two number of the year is 00, century stays as it is.
+		else {ctn++;} //if not, century should be plused one.
+		System.out.println(year+" is "+ctn+"th century."); //give out the result!
 	}
 	
 }
